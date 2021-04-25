64d146e9fb4b5be5838c066bc83a564a27dd1a3d
TenthDalek <anakaradu44@gmail.com>
2021-04-25 16:56:32 +0200

    SoundBank_Generated

--- LudumDare48/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ LudumDare48/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,90 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID DANGER = 4174463524U;
+        static const AkUniqueID DASH = 1942692385U;
+        static const AkUniqueID DEATH = 779278001U;
+        static const AkUniqueID INITMUSIC = 2984404912U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID OUTOF_DASH = 2054631987U;
+        static const AkUniqueID OUTOF_JUMP = 303116883U;
+        static const AkUniqueID PLAY_COLLECTIBLE = 661747096U;
+        static const AkUniqueID SLIDE_SIDE = 1953162098U;
+        static const AkUniqueID SLIDE_UP = 3385125720U;
+        static const AkUniqueID UI_BACK = 2024222415U;
+        static const AkUniqueID UI_OK = 1801758120U;
+        static const AkUniqueID W1L1 = 1558890612U;
+        static const AkUniqueID W1L2 = 1558890615U;
+        static const AkUniqueID W1L3 = 1558890614U;
+        static const AkUniqueID W2L1 = 2062366247U;
+        static const AkUniqueID W2L2 = 2062366244U;
+        static const AkUniqueID W2L3 = 2062366245U;
+        static const AkUniqueID W3L1 = 2028958134U;
+        static const AkUniqueID W3L2 = 2028958133U;
+        static const AkUniqueID W3L3 = 2028958132U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAME
+        {
+            static const AkUniqueID GROUP = 702482391U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAMEOVER = 4158285989U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TITLE = 3705726509U;
+                static const AkUniqueID WORLD01 = 2412061574U;
+                static const AkUniqueID WORLD02 = 2412061573U;
+                static const AkUniqueID WORLD03 = 2412061572U;
+            } // namespace STATE
+        } // namespace GAME
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace COLLECTIBLES
+        {
+            static const AkUniqueID GROUP = 4237018540U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BONUS = 2356001030U;
+                static const AkUniqueID DASH = 1942692385U;
+                static const AkUniqueID JUMP = 3833651337U;
+            } // namespace SWITCH
+        } // namespace COLLECTIBLES
+
+        namespace DANGER
+        {
+            static const AkUniqueID GROUP = 4174463524U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CHAINSAW = 3963162363U;
+                static const AkUniqueID LAVA = 540301611U;
+                static const AkUniqueID PIKE = 2028414480U;
+            } // namespace SWITCH
+        } // namespace DANGER
+
+        namespace LEVEL
+        {
+            static const AkUniqueID GROUP = 2782712965U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LV01 = 1684872406U;
+                static const AkUniqueID LV02 = 1684872405U;
+                static const AkUniqueID LV03 = 1684872404U;
+            } // namespace SWITCH
+        } // namespace LEVEL
+
+    } // namespace SWITCHES
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
